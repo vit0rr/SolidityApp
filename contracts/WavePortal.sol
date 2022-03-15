d2065ecf69b2761afefc415f697b56d57e890a7c
Vitor <vi.souza.almeida@protonmail.com>
2022-03-15 15:46:44 -0300

    feat: Log running in SC

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("I'm a smart contract and I'm ready to go!");
+    }
+}
